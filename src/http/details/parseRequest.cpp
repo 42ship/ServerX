155563f7722a62247b7ebb3df27462c5b90d6561
Oleh <g.r.i.t.s.i.v.o.l.e.g.95@gmail.com>
2025-09-16 15:33:00 +0100

    fix: Headers parsing breaks when header value is empty

--- src/http/details/parseRequest.cpp
+++ src/http/details/parseRequest.cpp
@@ -26,15 +26,14 @@ bool parseHeaderLine(string const &line, pair<string, string> &p) {
     if (column_pos == string::npos)
         return false;
     size_t key_start = line.find_first_not_of(" \t\r\n");
-    size_t key_end = line.find_last_not_of(" \t\r\n", column_pos - 1);
+    size_t key_end = line.find_last_not_of(" \t\r\n");
     if (key_start == string::npos || key_start > key_end)
         return false;
     size_t value_start = line.find_first_not_of(" \t\r\n", column_pos + 1);
-    size_t value_end = line.find_last_not_of(" \t\r\n");
-    if (value_start == string::npos || value_start > value_end)
+    if (value_start == string::npos || value_start > key_end)
         return false;
     p.first = line.substr(key_start, column_pos - key_start);
-    p.second = line.substr(value_start, value_end - value_start + 1);
+    p.second = line.substr(value_start, key_end - value_start + 1);
     return true;
 }
 
